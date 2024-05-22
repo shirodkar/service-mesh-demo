e2276bb86db540b8af15d31e321c74bfc920eaed
shirodkar <shirodkar.sachin@gmail.com>
2024-05-21 23:48:56 -0400

    Update ServiceOneResource.java

--- service-one/src/main/java/com/shirodkar/servicemeshdemo/serviceone/rest/ServiceOneResource.java
+++ service-one/src/main/java/com/shirodkar/servicemeshdemo/serviceone/rest/ServiceOneResource.java
@@ -11,12 +11,12 @@ public class ServiceOneResource {
     private final RestClient restClient;
 
     public ServiceOneResource(RestClient.Builder restClientBuilder) {
-		this.restClient = restClientBuilder.baseUrl("http://service-two").build();
+		this.restClient = restClientBuilder.baseUrl("http://service-two:8080").build();
     }
 
     @GetMapping("/handle/{value}")
     public String handleServiceOne(@PathVariable String value) {
-	    return this.restClient.get().uri("handle/{value}", value).retrieve().body(String.class);
+	    return this.restClient.get().uri("/handle/{value}", value).retrieve().body(String.class);
     }
     
 }
